d1167acaf9989681147bef9bdbbf56cacfc1701e
harish-f <harish_ram@s2021.ssts.edu.sg>
2022-10-12 19:48:01 +0800

    Initial Commit

---
+++ GoogleSAP/GoogleSAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleSAPApp.swift
+//  GoogleSAP
+//
+//  Created by HARISH RAM BAGHAVATH stu on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GoogleSAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
