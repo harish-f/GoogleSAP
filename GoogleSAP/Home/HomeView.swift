0fd4193ddc3aa4c1ff9e693d37d49072b721cb1f
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2022-10-13 14:48:28 +0800

    Completed First 1 Big Ring for the homepage

--- GoogleSAP/Home/HomeView.swift
+++ GoogleSAP/Home/HomeView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var progress = 0.0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack(alignment: .leading) {
+//              TODO: Make the ProgressViews data dependent on internal storage
+                VStack(alignment: .leading) {
+                    CircularProgressView(progress:progress, content: {
+                        CircularProgressView(progress: progress, content: {
+                            Button {
+                                progress += 0.1
+                            } label: {
+                                Text("Press me")
+                            }
+                        })
+                            .frame(width:UIScreen.main.bounds.width-150, height: UIScreen.main.bounds.width-50)
+                    })
+                        .frame(width:UIScreen.main.bounds.width-50, height: UIScreen.main.bounds.width-50)
+                        .padding(20)
+                    HStack {
+                        
+                    }
+                    Spacer()
+                }
+                .navigationTitle("Overview")
+            }
+        }
     }
 }
 
