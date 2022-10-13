15d75a85f999318b1a2b66d9f93ad6d0c983a7d7
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2022-10-13 09:23:02 +0800

    Added a new swiftUI component file for circular data preview

---
+++ GoogleSAP/Home/CircleStatsComponent.swift
@@ -0,0 +1,20 @@
+//
+//  CircleStatsComponent.swift
+//  GoogleSAP
+//
+//  Created by Sairam Suresh on 13/10/22.
+//
+
+import SwiftUI
+
+struct CircleStatsComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleStatsComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleStatsComponent()
+    }
+}
