3b8958eddd2a4fbe598bb252c928f1a0efbc4c0d
thamEnJie <tham_en_jie@s2021.ssts.edu.sg>
2022-11-14 21:45:00 +0800

    Decresed timing for wheel spin

--- GoogleSAP/Standards/ScoringStandardsView.swift
+++ GoogleSAP/Standards/ScoringStandardsView.swift
@@ -60,7 +60,7 @@ struct ScoringStandardsView: View {
                     var ref2 = ref
                     if selected+ref<0 { ref2 += stationsCount }
                     if selected+ref>=stationsCount { ref2 -= stationsCount }
-                    withAnimation(.spring(response: 0.25, dampingFraction: 0.5)) {
+                    withAnimation(.spring(response: 0.125, dampingFraction: 0.5)) {
                         scrollSelected += ref
                         selected += ref2
                     }
