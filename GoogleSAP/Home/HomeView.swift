7cd2e4345c9375ac4aab9d9fe2475134076eb423
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2022-11-21 22:04:04 +0800

    Major Redesign of the home screen with some bugs

--- GoogleSAP/Home/HomeView.swift
+++ GoogleSAP/Home/HomeView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftUISnappingScrollView
 
 
 struct ProgressData: Hashable {
@@ -22,6 +23,9 @@ struct ProgressData: Hashable {
 //  - Colour Scheme
 //  -
 
+struct idGen: Hashable {
+    var text: String
+}
 
 
 struct HomeView: View {
@@ -65,6 +69,9 @@ struct HomeView: View {
         ProgressData(text: "Shuttle Run", fraction: 0.0),
     ]
     
+    @State var sizeOfBigProgress = 0.0
+    
+    @State var heightOfNavView = 0.0
     
     var body: some View {
         GeometryReader { geometry in
@@ -73,62 +80,88 @@ struct HomeView: View {
                     // TODO: MAKE VIEWS ADAPT TO USER AGE
                     
                     VStack(alignment: .leading) {
-                        HStack {
-                            Spacer()
-                            CircularProgressViewLarge(progress: progress, content: {
-                                CircularProgressViewLarge(progress: Double(lastNAPFAElement.standingBroadJump)! / StandingBroadJumpHighestScore, content: {
-                                    Text("Standing Broad Jump").font(.title2)
-                                })
-                                .padding(.leading, 45)
-                                .padding(.trailing, 45)
-                            })
-                            .frame(width:UIScreen.main.bounds.width-50, height: UIScreen.main.bounds.width-50)
-                            Spacer()
-                        }
-                        .padding(.top, 10)
                         
                         
-                        HStack {
-                            Spacer()
-                            
-                            ForEach(secondRow, id: \.self) { theLists in
-                                CircularProgressViewSmall(progress: 0) {
-                                    CircularProgressViewSmall(progress: theLists.fraction) {
-                                        Text(theLists.text).multilineTextAlignment(.center)
-                                    }
-                                    .padding(SMALL_PROGRESS_VIEW_PADDING)
-                                }
-                                .frame(width: geometry.size.width/3-25, height: geometry.size.width/3-25)
+                        ScrollViewReader { proxy in
+                            SnappingScrollView(.horizontal, decelerationRate: .fast, showsIndicators: false) {
+                                
                                 Spacer()
-                            }
-                        }
-                        .padding(.leading, 3)
-                        .padding(.trailing, 3)
-                        .padding(.top, 20)
-                        
-                        HStack {
-                            Spacer()
-                            
-                            ForEach(thirdRow, id: \.self) { text in
-                                CircularProgressViewSmall(progress: 0.0) {
-                                    CircularProgressViewSmall(progress: text.fraction) {
-                                        Text(text.text)
-                                            .multilineTextAlignment(.center)
+                                
+                                HStack(alignment: .center) {
+                                    VStack(alignment: .center) {
+                                        CircularProgressViewLarge(progress: progress, content: {
+                                            CircularProgressViewLarge(progress: Double(lastNAPFAElement.standingBroadJump)! / StandingBroadJumpHighestScore, content: {
+                                                Button {
+                                                    proxy.scrollTo(idGen(text: "2st"))
+                                                } label: {
+                                                    Text("Standing Broad Jump").font(.title3)
+                                                }
+                                            })
+                                            .padding(.leading, 45)
+                                            .padding(.trailing, 45)
+                                        })
+                                        .frame(width:UIScreen.main.bounds.width-50, height: UIScreen.main.bounds.width-50, alignment: .center)
+                                        .padding(.bottom, geometry.size.height * 0.03)
                                     }
-                                    .padding(SMALL_PROGRESS_VIEW_PADDING)
                                 }
-                                .frame(width: geometry.size.width/3-15, height: geometry.size.width/3-15)
+                                .id(idGen(text: "1st"))
+                                .padding(.top, 10)
+                                .padding()
+                                .background(
+                                    GeometryReader { geometryProxy in
+                                        Color.clear
+                                            .onAppear {
+                                                sizeOfBigProgress = geometryProxy.size.height
+                                            }
+                                    }
+                                )
+                                
                                 Spacer()
+                                
+                                
+                                HStack(alignment: .center) {
+                                    VStack(alignment: .center) {
+                                        CircularProgressViewLarge(progress: progress, content: {
+                                            CircularProgressViewLarge(progress: Double(lastNAPFAElement.standingBroadJump)! / StandingBroadJumpHighestScore, content: {
+                                                Text("Standing Broad Jump").font(.title3)
+                                            })
+                                            .padding(.leading, 45)
+                                            .padding(.trailing, 45)
+                                        })
+                                        .frame(width:UIScreen.main.bounds.width-50, height: UIScreen.main.bounds.width-50, alignment: .center)
+                                        .padding(.bottom, geometry.size.height * 0.04)
+                                    }
+                                }
+                                .padding(.top, 10)
+                                .padding()
+                                .background(
+                                    GeometryReader { geometryProxy in
+                                        Color.clear
+                                            .onAppear {
+                                                sizeOfBigProgress = geometryProxy.size.height
+                                            }
+                                    }
+                                )
+                                
+                                Spacer()
+                                    .id(idGen(text: "2st"))
                             }
+                            .padding(.bottom, sizeOfBigProgress * 0.04)
                         }
-                        .padding(.leading, 3)
-                        .padding(.trailing, 3)
-                        .padding(.top, 10)
                         
-                        Spacer()
                         
+                        
+                        CustomScrollView(sizeOfProgBar: $heightOfNavView)
+                            .frame(height: heightOfNavView)
+                        
+                        
+                        Form {
+                            Text("helo wolrd")
+                        }
+                        .padding(.top, sizeOfBigProgress * 0.07)
                         
                     }.navigationTitle("Overview")
+                        .navigationBarTitleDisplayMode(.inline)
                 }
             }.onAppear {
                 loggerHistoryManager.loadData()
@@ -152,8 +185,141 @@ struct HomeView: View {
     }
 }
 
+
+struct CustomScrollView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
+    @State var selectedRing = 0
+    @State var geometry = CGSize(width: 0.0, height: 0.0)
+    
+    @Binding var sizeOfProgBar: Double
+    
+    var body: some View {
+        ZStack(alignment: .center) {
+            GeometryReader {geo in
+                HStack {
+                    
+                }.onAppear {
+                    geometry = geo.size
+                }
+            }
+            ZStack {
+                RoundedRectangle(cornerRadius: 50, style: .continuous)
+                    .fill(colorScheme == .dark ? .white : .black)
+                    .frame(width: geometry.width / 2, height: geometry.width / 10)
+                HStack {
+                    CustomProgressView(geometry: geometry, Selection: $selectedRing, index: 0)
+                    
+                    Spacer()
+                    
+                    CustomProgressView(geometry: geometry, Selection: $selectedRing, index: 1)
+                    
+                    Spacer()
+                    
+                    CustomProgressView(geometry: geometry, Selection: $selectedRing, index: 2)
+                    
+                }.frame(width: geometry.width / 2.5, height: geometry.width / 11)
+            }
+            .background {
+                GeometryReader { geometryProxy in
+                    Color.clear
+                        .onAppear {
+                            sizeOfProgBar = geometryProxy.size.height
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct CustomProgressView: View {
+    var geometry: CGSize
+    @Binding var Selection: Int
+    var index: Int
+    
+    var body: some View {
+        ZStack(alignment:.center) {
+            Circle()
+                .fill(Selection == index ? .white : .clear)
+                .frame(width: geometry.width / 17, height: geometry.width / 17)
+            Circle()
+                .stroke(
+                    Color.red.opacity(0.7),
+                    lineWidth: CGFloat(10)
+                )
+            
+            Circle() // 2
+                .trim(from: 0, to: 0.25)
+                .stroke(
+                    Color.green,
+                    // 1
+                    style: StrokeStyle(
+                        lineWidth: CGFloat(10),
+                        lineCap: .round
+                    )
+                )
+                .rotationEffect(.degrees(-90))
+                .animation(.easeOut, value: 0.25)
+        }
+        .frame(width: geometry.width / 18, height: geometry.width / 18)
+        .onTapGesture {
+            withAnimation {
+                Selection = index
+            }
+            
+        }
+    }
+}
+
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
     }
 }
+
+//struct CustomScrollView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        VStack(alignment: .center) {
+//            Spacer()
+//            CustomScrollView(sizeOfProgBar: )
+//        }
+//    }
+//}
+
+//
+//HStack {
+//    Spacer()
+//
+//    ForEach(secondRow, id: \.self) { theLists in
+//        CircularProgressViewSmall(progress: 0) {
+//            CircularProgressViewSmall(progress: theLists.fraction) {
+//                Text(theLists.text).multilineTextAlignment(.center)
+//            }
+//            .padding(SMALL_PROGRESS_VIEW_PADDING)
+//        }
+//        .frame(width: geometry.size.width/3-25, height: geometry.size.width/3-25)
+//        Spacer()
+//    }
+//}
+//.padding(.leading, 3)
+//.padding(.trailing, 3)
+//.padding(.top, 20)
+//
+//HStack {
+//    Spacer()
+//
+//    ForEach(thirdRow, id: \.self) { text in
+//        CircularProgressViewSmall(progress: 0.0) {
+//            CircularProgressViewSmall(progress: text.fraction) {
+//                Text(text.text)
+//                    .multilineTextAlignment(.center)
+//            }
+//            .padding(SMALL_PROGRESS_VIEW_PADDING)
+//        }
+//        .frame(width: geometry.size.width/3-15, height: geometry.size.width/3-15)
+//        Spacer()
+//    }
+//}
+//.padding(.leading, 3)
+//.padding(.trailing, 3)
+//.padding(.top, 10)
