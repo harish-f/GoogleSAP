e9afdf25a90d45906574a5f36787075932870b1e
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2022-11-21 23:14:30 +0800

    Completed important parts of homepage and added support for workouts in the outer ring

--- GoogleSAP/Home/HomeView.swift
+++ GoogleSAP/Home/HomeView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 import SwiftUISnappingScrollView
 
 
-struct ProgressData: Hashable {
+struct ProgressData: Hashable, Identifiable {
     var text: String
-    var fraction: Double
+    var fractionNAPFA: Double
+    var fractionWorkout: Double
+    var id = UUID()
 }
 
 
@@ -56,18 +58,20 @@ struct HomeView: View {
         standingBroadJump: "0.6"
     )
     
-    // Data in here is just placeholder (variable data declared in view .onAppear)
-    @State var secondRow: [ProgressData] = [
-        ProgressData(text: "2.4 Run", fraction: 0.0),
-        ProgressData(text: "Situps", fraction: 0.0),
-        ProgressData(text: "Inclined Pullups",fraction: 0.0),
-    ]
+    @State var lastWorkoutElement: LogRecord = LogRecord(
+        NapfaOrWorkouts: .napfa,
+        description: "This is my description",
+        date: Date(timeInterval: .zero, since: .now),
+        twoPointFourKMRun: "0.1",
+        shuttleRun: "0.2",
+        sitUps: "0.3",
+        sitAndReach: "0.4",
+        inclinedPullups: "0.5",
+        standingBroadJump: "0.6"
+    )
     
     // Data in here is just placeholder (variable data declared in view .onAppear)
-    @State var thirdRow: [ProgressData] = [
-        ProgressData(text: "Sit & Reach", fraction: 0.0),
-        ProgressData(text: "Shuttle Run", fraction: 0.0),
-    ]
+    @State var data: [ProgressData] = []
     
     @State var sizeOfBigProgress = 0.0
     
@@ -83,193 +87,201 @@ struct HomeView: View {
                         
                         
                         ScrollViewReader { proxy in
-                            SnappingScrollView(.horizontal, decelerationRate: .fast, showsIndicators: false) {
-                                
-                                Spacer()
-                                
-                                HStack(alignment: .center) {
-                                    VStack(alignment: .center) {
-                                        CircularProgressViewLarge(progress: progress, content: {
-                                            CircularProgressViewLarge(progress: Double(lastNAPFAElement.standingBroadJump)! / StandingBroadJumpHighestScore, content: {
-                                                Button {
-                                                    proxy.scrollTo(idGen(text: "2st"))
-                                                } label: {
-                                                    Text("Standing Broad Jump").font(.title3)
+                            SnappingScrollView(.horizontal, decelerationRate: .fast, showsIndicators: true) {
+                                ForEach(data) { datum in
+                                    Spacer()
+                                    HStack(alignment: .center) {
+                                        VStack(alignment: .center) {
+                                            CircularProgressViewLarge(progress: datum.fractionWorkout, content: {
+                                                CircularProgressViewLarge(progress: datum.fractionNAPFA, content: {
+                                                    Button {
+                                                        proxy.scrollTo(idGen(text: datum.text))
+                                                    } label: {
+                                                        Text(datum.text).font(.title3)
+                                                    }
+                                                })
+                                                .padding(.leading, 45)
+                                                .padding(.trailing, 45)
+                                            })
+                                            .frame(width:UIScreen.main.bounds.width-50, height: UIScreen.main.bounds.width-50, alignment: .center)
+                                            .padding(.bottom, geometry.size.height * 0.03)
+                                        }
+                                    }
+                                    .padding(.top, 10)
+                                    .padding()
+                                    .background(
+                                        GeometryReader { geometryProxy in
+                                            Color.clear
+                                                .onAppear {
+                                                    sizeOfBigProgress = geometryProxy.size.height
                                                 }
-                                            })
-                                            .padding(.leading, 45)
-                                            .padding(.trailing, 45)
-                                        })
-                                        .frame(width:UIScreen.main.bounds.width-50, height: UIScreen.main.bounds.width-50, alignment: .center)
-                                        .padding(.bottom, geometry.size.height * 0.03)
-                                    }
+                                        }
+                                    )
+                                    
+                                    Spacer()
+                                        .scrollSnappingAnchor(.bounds)
+                                        .id(idGen(text: datum.text))
                                 }
-                                .id(idGen(text: "1st"))
-                                .padding(.top, 10)
-                                .padding()
-                                .background(
-                                    GeometryReader { geometryProxy in
-                                        Color.clear
-                                            .onAppear {
-                                                sizeOfBigProgress = geometryProxy.size.height
-                                            }
-                                    }
-                                )
-                                
-                                Spacer()
-                                
-                                
-                                HStack(alignment: .center) {
-                                    VStack(alignment: .center) {
-                                        CircularProgressViewLarge(progress: progress, content: {
-                                            CircularProgressViewLarge(progress: Double(lastNAPFAElement.standingBroadJump)! / StandingBroadJumpHighestScore, content: {
-                                                Text("Standing Broad Jump").font(.title3)
-                                            })
-                                            .padding(.leading, 45)
-                                            .padding(.trailing, 45)
-                                        })
-                                        .frame(width:UIScreen.main.bounds.width-50, height: UIScreen.main.bounds.width-50, alignment: .center)
-                                        .padding(.bottom, geometry.size.height * 0.04)
-                                    }
-                                }
-                                .padding(.top, 10)
-                                .padding()
-                                .background(
-                                    GeometryReader { geometryProxy in
-                                        Color.clear
-                                            .onAppear {
-                                                sizeOfBigProgress = geometryProxy.size.height
-                                            }
-                                    }
-                                )
-                                
-                                Spacer()
-                                    .id(idGen(text: "2st"))
+
                             }
-                            .padding(.bottom, sizeOfBigProgress * 0.04)
+                            .padding(.bottom, sizeOfBigProgress * 0.000)
                         }
                         
                         
                         
-                        CustomScrollView(sizeOfProgBar: $heightOfNavView)
-                            .frame(height: heightOfNavView)
+//                        CustomScrollView(sizeOfProgBar: $heightOfNavView)
+//                            .frame(height: heightOfNavView)
                         
                         
                         Form {
                             Text("helo wolrd")
                         }
-                        .padding(.top, sizeOfBigProgress * 0.07)
+                        .padding(.top, sizeOfBigProgress * 0.00)
                         
                     }.navigationTitle("Overview")
                         .navigationBarTitleDisplayMode(.inline)
                 }
             }.onAppear {
                 loggerHistoryManager.loadData()
+                
                 lastNAPFAElement = loggerHistoryManager.logRecords.last { LogRecord in
                     LogRecord.napfaOrWorkout == "Napfa"
-                }!
-                
-                secondRow = [
-                    ProgressData(text: "2.4 Run", fraction: TwoPointFourKMRunHighestScore / Double(lastNAPFAElement.twoPointFourKMRun)!),
-                    ProgressData(text: "Situps", fraction: Double(lastNAPFAElement.sitUps)! / SitUpsHighestScore),
-                    ProgressData(text: "Inclined Pullups", fraction: Double(lastNAPFAElement.standingBroadJump)! / StandingBroadJumpHighestScore),
-                ]
-                
-                thirdRow = [
-                    ProgressData(text: "Sit & Reach", fraction: Double(lastNAPFAElement.sitAndReach)! / SitAndReachHighestScore),
-                    ProgressData(text: "Shuttle Run", fraction: ShuttleRunHighestScore / Double(lastNAPFAElement.shuttleRun)!),
-                ]
-                
-            }
-        }
-    }
-}
-
-
-struct CustomScrollView: View {
-    @Environment(\.colorScheme) var colorScheme
-    
-    @State var selectedRing = 0
-    @State var geometry = CGSize(width: 0.0, height: 0.0)
-    
-    @Binding var sizeOfProgBar: Double
-    
-    var body: some View {
-        ZStack(alignment: .center) {
-            GeometryReader {geo in
-                HStack {
-                    
-                }.onAppear {
-                    geometry = geo.size
-                }
-            }
-            ZStack {
-                RoundedRectangle(cornerRadius: 50, style: .continuous)
-                    .fill(colorScheme == .dark ? .white : .black)
-                    .frame(width: geometry.width / 2, height: geometry.width / 10)
-                HStack {
-                    CustomProgressView(geometry: geometry, Selection: $selectedRing, index: 0)
-                    
-                    Spacer()
-                    
-                    CustomProgressView(geometry: geometry, Selection: $selectedRing, index: 1)
-                    
-                    Spacer()
-                    
-                    CustomProgressView(geometry: geometry, Selection: $selectedRing, index: 2)
-                    
-                }.frame(width: geometry.width / 2.5, height: geometry.width / 11)
-            }
-            .background {
-                GeometryReader { geometryProxy in
-                    Color.clear
-                        .onAppear {
-                            sizeOfProgBar = geometryProxy.size.height
-                    }
-                }
-            }
-        }
-    }
-}
-
-struct CustomProgressView: View {
-    var geometry: CGSize
-    @Binding var Selection: Int
-    var index: Int
-    
-    var body: some View {
-        ZStack(alignment:.center) {
-            Circle()
-                .fill(Selection == index ? .white : .clear)
-                .frame(width: geometry.width / 17, height: geometry.width / 17)
-            Circle()
-                .stroke(
-                    Color.red.opacity(0.7),
-                    lineWidth: CGFloat(10)
+                } ?? LogRecord(
+                    NapfaOrWorkouts: .napfa,
+                    description: "This is my description",
+                    date: Date(timeInterval: .zero, since: .now),
+                    twoPointFourKMRun: "0",
+                    shuttleRun: "0",
+                    sitUps: "0",
+                    sitAndReach: "0",
+                    inclinedPullups: "0",
+                    standingBroadJump: "0"
                 )
-            
-            Circle() // 2
-                .trim(from: 0, to: 0.25)
-                .stroke(
-                    Color.green,
-                    // 1
-                    style: StrokeStyle(
-                        lineWidth: CGFloat(10),
-                        lineCap: .round
-                    )
+                
+                lastWorkoutElement = loggerHistoryManager.logRecords.last { LogRecord in
+                    LogRecord.NapfaOrWorkouts == .workout
+                } ?? LogRecord(
+                    NapfaOrWorkouts: .workout,
+                    description: "This is my description",
+                    date: Date(timeInterval: .zero, since: .now),
+                    twoPointFourKMRun: "0",
+                    shuttleRun: "0",
+                    sitUps: "0",
+                    sitAndReach: "0",
+                    inclinedPullups: "0",
+                    standingBroadJump: "0"
                 )
-                .rotationEffect(.degrees(-90))
-                .animation(.easeOut, value: 0.25)
-        }
-        .frame(width: geometry.width / 18, height: geometry.width / 18)
-        .onTapGesture {
-            withAnimation {
-                Selection = index
+                
+                data = [
+                    ProgressData(text: "2.4 Run",
+                                 fractionNAPFA: TwoPointFourKMRunHighestScore / Double(lastNAPFAElement.twoPointFourKMRun)!,
+                                 fractionWorkout: TwoPointFourKMRunHighestScore / Double(lastWorkoutElement.twoPointFourKMRun)!),
+                    ProgressData(text: "Situps",
+                                 fractionNAPFA: Double(lastNAPFAElement.sitUps)! / SitUpsHighestScore, fractionWorkout: Double(lastWorkoutElement.sitUps)! / SitUpsHighestScore),
+                    ProgressData(text: "Inclined Pullups",
+                                 fractionNAPFA: Double(lastNAPFAElement.inclinedPullups)! / StandingBroadJumpHighestScore,
+                                 fractionWorkout: Double(lastWorkoutElement.inclinedPullups)! / StandingBroadJumpHighestScore),
+                    ProgressData(text: "Sit & Reach",
+                                 fractionNAPFA: Double(lastNAPFAElement.sitAndReach)! / SitAndReachHighestScore,
+                                 fractionWorkout: Double(lastWorkoutElement.sitAndReach)! / SitAndReachHighestScore),
+                    ProgressData(text: "Shuttle Run",
+                                 fractionNAPFA: ShuttleRunHighestScore / Double(lastNAPFAElement.shuttleRun)!,
+                                 fractionWorkout: ShuttleRunHighestScore / Double(lastWorkoutElement.shuttleRun)!),
+                    ProgressData(text: "Standing Broad Jump",
+                                 fractionNAPFA: Double(lastNAPFAElement.standingBroadJump)! / StandingBroadJumpHighestScore, fractionWorkout: Double(lastWorkoutElement.standingBroadJump)! / StandingBroadJumpHighestScore)
+                ]
             }
-            
         }
     }
 }
+
+
+//struct CustomScrollView: View {
+//    @Environment(\.colorScheme) var colorScheme
+//
+//    @State var selectedRing = 0
+//    @State var geometry = CGSize(width: 0.0, height: 0.0)
+//
+//    @Binding var sizeOfProgBar: Double
+//
+//    var body: some View {
+//        ZStack(alignment: .center) {
+//            GeometryReader {geo in
+//                HStack {
+//
+//                }.onAppear {
+//                    geometry = geo.size
+//                }
+//            }
+//
+//            ZStack {
+//                Rectangle()
+//                    .fill(colorScheme == .dark ? .white : .black)
+//                    .frame(width: geometry.width, height: geometry.width / 8)
+//                HStack {
+//                    CustomProgressView(geometry: geometry, Selection: $selectedRing, index: 0)
+//
+//                    Spacer()
+//
+//                    CustomProgressView(geometry: geometry, Selection: $selectedRing, index: 1)
+//
+//                    Spacer()
+//
+//                    CustomProgressView(geometry: geometry, Selection: $selectedRing, index: 2)
+//
+//                }.frame(width: geometry.width / 2.5, height: geometry.width / 11)
+//            }
+//            .background {
+//                GeometryReader { geometryProxy in
+//                    Color.clear
+//                        .onAppear {
+//                            sizeOfProgBar = geometryProxy.size.height
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//struct CustomProgressView: View {
+//    @Environment(\.colorScheme) var colorScheme
+//    var geometry: CGSize
+//    @Binding var Selection: Int
+//    var index: Int
+//
+//    var body: some View {
+//        ZStack(alignment:.center) {
+//            Circle()
+//                .fill(Selection == index ? colorScheme == .dark ? .black : .white : .clear)
+//                .frame(width: geometry.width / 17, height: geometry.width / 17)
+//            Circle()
+//                .stroke(
+//                    Color.red.opacity(0.7),
+//                    lineWidth: CGFloat(10)
+//                )
+//
+//            Circle() // 2
+//                .trim(from: 0, to: 0.25)
+//                .stroke(
+//                    Color.green,
+//                    // 1
+//                    style: StrokeStyle(
+//                        lineWidth: CGFloat(10),
+//                        lineCap: .round
+//                    )
+//                )
+//                .rotationEffect(.degrees(-90))
+//                .animation(.easeOut, value: 0.25)
+//        }
+//        .frame(width: geometry.width / 18, height: geometry.width / 18)
+//        .onTapGesture {
+//            withAnimation {
+//                Selection = index
+//            }
+//
+//        }
+//    }
+//}
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
