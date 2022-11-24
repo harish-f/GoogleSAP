3f5c3ee1e6afe2c015fc818e8afdd23ceaca6074
thamEnJie <tham_en_jie@s2021.ssts.edu.sg>
2022-11-24 21:56:35 +0800

    hopefully fixed file problems

--- GoogleSAP/Standards/CalculatorStartPageView.swift
+++ GoogleSAP/Standards/CalculatorStartPageView.swift
@@ -13,26 +13,23 @@ struct CalculatorStartPageView: View {
     @State var genderInput: gender = .gender
     @State var pageType: calcPage = .none
     
+    @State var insideViewGeometry: CGSize = CGSize(width: 0.0, height: 0.0)
+    @State var dragOffset: CGFloat = .zero
+    
     var body: some View {
         VStack {
                 HStack(spacing: 0) {
                     Text("I am a")
                     Picker("Age", selection: $ageInput) {
                         Text("Age").tag(0)
-                        ForEach(9...60, id: \.self) {
+                        ForEach(9...19, id: \.self) { //updated for napfa only, ippt up to age 60
                             Text(String($0)).tag($0)
                         }
-                    }
+                    }.frame(maxWidth: .infinity)
                         .overlay {
                             VStack {
                                 Spacer()
-                                HStack {
-                                    Spacer()
-                                    Spacer()
-                                    Rectangle().fill(ageInput == 0 ? .red:.clear).frame(height: 2)
-                                    Spacer()
-                                    Spacer()
-                                }
+                                Rectangle().fill(ageInput == 0 ? .red:.clear).frame(height: 2).padding(.horizontal).padding(.horizontal)
                             }
                         }
                     Text("year old")
@@ -40,20 +37,14 @@ struct CalculatorStartPageView: View {
                         ForEach(gender.allCases, id: \.self) {
                             Text($0.rawValue.capitalized)
                         }
-                    }
+                    }.frame(maxWidth: .infinity)
                         .overlay {
                             VStack {
                                 Spacer()
-                                HStack {
-                                    Spacer()
-                                    Spacer()
-                                    Rectangle().fill(genderInput == .gender ? .red:.clear).frame(height: 2)
-                                    Spacer()
-                                    Spacer()
-                                }
+                                Rectangle().fill(genderInput == .gender ? .red:.clear).frame(height: 2).padding(.horizontal).padding(.horizontal)
                             }
                         }
-                }.pickerStyle(.menu).padding()
+                }.pickerStyle(.menu).frame(maxWidth: .infinity).padding().padding(.horizontal)
             if pageType == calcPage.none || ageInput == 0 || genderInput == .gender { //should switch to switch case?
                 ForEach(calcPage.allCases, id: \.self) { page in
                     if page != calcPage.none {
@@ -65,15 +56,43 @@ struct CalculatorStartPageView: View {
                 }.onAppear {
                     withAnimation{ pageType = .none }
                 }
-            } else if pageType == calcPage.standards {
-                Spacer()
-                ScoringStandardsView(ageInput: $ageInput, genderInput: $genderInput)
-                Spacer()
-            } else if pageType == calcPage.calculate {
-                Spacer()
-                GradeScoreCaclulator()
-                Spacer()
-            }//add pull down to reset age and gender
+            } else {
+                ZStack {
+                    GeometryReader{ geometry in
+                        Spacer().onAppear { insideViewGeometry = geometry.size }
+                    }
+                    VStack {
+                        Spacer()
+                        Group {
+                            if pageType == calcPage.standards {
+                                ScoringStandardsView(ageInput: $ageInput, genderInput: $genderInput)
+                            } else if pageType == calcPage.calculate {
+                                GradeScoreCaclulator()
+                            }
+                        }
+                        .offset(y: dragOffset)
+                        .gesture(DragGesture()
+                            .onChanged({ drag in
+                                dragOffset = drag.translation.height>0 ? drag.translation.height:CGFloat.zero
+                            })
+                            .onEnded({ drag in
+                                if drag.translation.height>insideViewGeometry.height*0.66 {
+                                    withAnimation(.spring(response: 0.25)){
+                                        dragOffset = insideViewGeometry.height
+                                    }
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
+                                        withAnimation(.spring(response: 0.25)){
+                                            pageType = .none
+                                        }
+                                        DispatchQueue.main.asyncAfter(deadline: .now()+0.25) { dragOffset = .zero }
+                                    }
+                                } else { withAnimation(.spring()){ dragOffset = CGFloat.zero } }
+                            })
+                        )
+                        Spacer()
+                    }
+                }
+            }
         }
     }
 }
