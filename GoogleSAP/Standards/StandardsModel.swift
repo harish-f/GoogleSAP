1ae5e4c43a85cad81f392a636e0add51bc76b9b2
thamEnJie <tham_en_jie@s2021.ssts.edu.sg>
2022-11-24 15:38:10 +0800

    half complete StandardsModel

---
+++ GoogleSAP/Standards/StandardsModel.swift
@@ -0,0 +1,323 @@
+//
+//  StandardsModel.swift
+//  GoogleSAP
+//
+//  Created by Tham En Jie on 24/11/22.
+//
+
+import Foundation
+
+let NAPFAStandards: [gender:[Int:[Workout:[Int]]]] = [
+    .male: [
+        9: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 14, 19, 24, 29, 35 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 129, 138, 148, 158, 169 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 8, 15, 22, 29, 36 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 1, 4, 8, 17, 26 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        10: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 16, 20, 25, 30, 36 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 136, 145, 155, 164, 174 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 10, 17, 24, 31, 38 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 1, 5, 9, 18, 27 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        11: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 19, 24, 29, 33, 39 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 143, 154, 165, 176, 188 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 12, 19, 26, 33, 40 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 1, 5, 9, 18, 27 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        12: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 21, 26, 31, 35, 41 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 149, 162, 175, 188, 202 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 14, 21, 28, 35, 42 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 1, 5, 9, 18, 27 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        13: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 24, 28, 33, 37, 42 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 163, 175, 188, 201, 214 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 16, 23, 30, 37, 44 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 2, 6, 10, 19, 28 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        14: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        15: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        16: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        17: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        18: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        19: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+    ],
+    .female: [
+        9: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 9, 13, 17, 21, 26 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 118, 128, 138, 147, 158 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 10, 17, 24, 31, 37 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 1, 4, 8, 15, 22 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        10: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 10, 14, 18, 22, 27 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 124, 133, 142, 151, 161 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 12, 19, 26, 33, 39 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 1, 3, 7, 14, 21 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        11: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 11, 15, 19, 23, 28 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 128, 137, 146, 155, 164 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 14, 21, 28, 35, 41 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 1, 2, 6, 13, 20 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        12: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 12, 16, 20, 24, 29 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 131, 140, 149, 158, 167 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 16, 23, 30, 37, 43 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 2, 5, 12, 19 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        13: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 13, 17, 21, 25, 30 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 134, 143, 152, 161, 170 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 18, 25, 32, 39, 45 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 1, 3, 10, 17 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        14: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        15: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        16: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        17: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        18: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+        19: [
+            Stations["NAPFA"]![0]:  // Sit-ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![1]:  // SBJ
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![2]:  // Sit & Reach
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![3]:  // (Inclined) Pull-Ups
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![4]:  // Shuttle Run
+                [ 0, 0, 0, 0, 0 ],
+            Stations["NAPFA"]![5]:  // 1.6km / 2.4km
+                [ 0, 0, 0, 0, 0 ],
+        ],
+    ]
+]
